580c83227e0a33d5449e036aa568a619094821ea
dande7lion <halbiniak.natalia@gmail.com>
2021-01-19 22:56:23 +0100

    Added vertices and edges functions

--- graph.py
+++ graph.py
@@ -22,15 +22,53 @@ class Graph:
         v1 = self.find_vertex(vertex1)
         v2 = self.find_vertex(vertex2)
         if v1 == -1 or v2 == -1:
-            print("No vertex!")
+            print("Incorrect vertices")
             return
         self.edges[v1][v2].edge = "1"
         self.edges[v2][v1].edge = "1"
         self.number_of_edges = self.number_of_edges + 1
 
-    def find_vertex(self, vertex):
+    def find_vertex(self, vertex) -> int:
         for v in range(self.number_of_edges):
-            if self.vertices
+            if self.vertices[v].vertex == vertex:
+                return v
+        return -1
+
+    def set_vertex_value(self, vertex, value) -> None:
+        v = self.find_vertex(vertex)
+        if v == -1:
+            print(f"There is no vertex {vertex}")
+            return
+        self.vertices[v].value = value
+
+    def get_vertex_value(self, vertex) -> int:
+        v = self.find_vertex(vertex)
+        if v != -1:
+            return self.vertices[v].value
+        print(f"There is no vertex {vertex}")
+        return -1111111
+
+    def adjacent(self, vertex1, vertex2) -> bool:
+        v1 = self.find_vertex(vertex1)
+        v2 = self.find_vertex(vertex2)
+        if v1 == -1 or v2 == -1:
+            print("Incorrect vertices")
+            return False
+        if self.edges[v1][v2].edge == ".":
+            return False
+        return True
+
+    def set_edge_value(self, vertex1, vertex2, value) -> None:
+        v1 = self.find_vertex(vertex1)
+        v2 = self.find_vertex(vertex2)
+        if v1 == -1 or v2 == -1:
+            print("Incorrect vertices")
+            return
+        self.edges[v1][v2].value = value
+        self.edges[v2][v1].value = value
+
+    
+    
 
     
 
