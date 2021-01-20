b1f004184b86b9469d52c8d6da37468cb9539de4
dande7lion <halbiniak.natalia@gmail.com>
2021-01-20 21:29:30 +0100

    Added create_graph function and example main.py file

--- graph.py
+++ graph.py
@@ -4,8 +4,8 @@ Vertex = namedtuple('Vertex', ['vertex', 'value'])
 Edge = namedtuple('Edge', ['edge', 'value'])
 
 class Graph:
-    def __init__(self, number_of_teams):
-        self.number_of_teams = number_of_teams
+    def __init__(self):
+        self.number_of_teams = 50
         self.number_of_vertices = 0
         self.number_of_edges = 0
         self.vertices = []
@@ -14,11 +14,11 @@ class Graph:
             for j in range(self.number_of_teams):
                 self.edges[i][j] = Vertex(".", 0)
 
-    def add_vertex(self, new_vertex) -> None:
+    def add_vertex(self, new_vertex: str) -> None:
         self.vertices.append(Vertex(new_vertex, "-1"))
         self.number_of_vertices = self.number_of_vertices + 1
 
-    def add_edge(self, vertex1, vertex2) -> None:
+    def add_edge(self, vertex1: str, vertex2: str) -> None:
         v1 = self.find_vertex(vertex1)
         v2 = self.find_vertex(vertex2)
         if v1 == -1 or v2 == -1:
@@ -28,27 +28,27 @@ class Graph:
         self.edges[v2][v1].edge = "1"
         self.number_of_edges = self.number_of_edges + 1
 
-    def find_vertex(self, vertex) -> int:
+    def find_vertex(self, vertex: str) -> int:
         for v in range(self.number_of_edges):
             if self.vertices[v].vertex == vertex:
                 return v
         return -1
 
-    def set_vertex_value(self, vertex, value) -> None:
+    def set_vertex_value(self, vertex: str, value: int) -> None:
         v = self.find_vertex(vertex)
         if v == -1:
             print(f"There is no vertex {vertex}")
             return
         self.vertices[v].value = value
 
-    def get_vertex_value(self, vertex) -> int:
+    def get_vertex_value(self, vertex: str) -> int:
         v = self.find_vertex(vertex)
         if v != -1:
             return self.vertices[v].value
         print(f"There is no vertex {vertex}")
         return -1111111
 
-    def adjacent(self, vertex1, vertex2) -> bool:
+    def adjacent(self, vertex1: str, vertex2: str) -> bool:
         v1 = self.find_vertex(vertex1)
         v2 = self.find_vertex(vertex2)
         if v1 == -1 or v2 == -1:
@@ -58,7 +58,7 @@ class Graph:
             return False
         return True
 
-    def set_edge_value(self, vertex1, vertex2, value) -> None:
+    def set_edge_value(self, vertex1: str, vertex2: str, value: int) -> None:
         v1 = self.find_vertex(vertex1)
         v2 = self.find_vertex(vertex2)
         if v1 == -1 or v2 == -1:
@@ -68,8 +68,43 @@ class Graph:
         self.edges[v2][v1].value = value
 
     
-    
+    # n - number of teams
+    # names - names of teams
+    def create_graph(self, n: int, names: list):
 
-    
+        graph = Graph()
+        tmp = ''
+        value = 0
 
+        for i in range(n-1):
+            for j in range(n-1):
+                tmp = names[i] + ' - ' + names[j]
+                graph.add_vertex(tmp)
+                graph.set_vertex_value(tmp, value)
+                value = value + 1
+
+        first_team = ''
+        second_team = ''
+        new_team = ''
+        value = 0
+        index = 0
+        index2 = 0
+
+        for i in range(self.number_of_vertices):
+            index = graph.vertices[i].find(" ")
+            first_team = graph.vertices[i].vertex[0:index]
+            new_team = graph.vertices[i].vertex[index+3:]
+            for j in range(self.number_of_vertices):
+                if i == j:
+                    continue
+                second_team = (graph.vertices[j].vertex)[0:index]
+                if(firstTeam == secondTeam or newTeam == secondTeam):
+                    graph.add_edge(graph.vertices[i].vertex, graph.vertices[j].vertex)
+                else:
+                    index2 = (graph.vertices[j].vertex).find(" ")
+                    secondTeam = (graph.vertices[j].vertex)[index2+3:]
+                    if(firstTeam == secondTeam or newTeam == secondTeam):
+                        graph.addEdge(graph.vertices[i].vertex, graph.vertices[j].vertex)
+
+                
 
---
+++ main.py
@@ -0,0 +1,4 @@
+from graph import Graph
+
+my_graph = Graph()
+my_graph = my_graph.create_graph(5, ['A', 'B', 'C'])
